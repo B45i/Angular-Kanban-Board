da5bb74350d725e218050fe33ed606d15e43cc79
b45i <amalshajan2011@gmail.com>
2021-11-18 17:44:53 +0530

    using local storage

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,35 +1,56 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 
+type Board = {
+  todo: Array<string>;
+  doing: Array<string>;
+  done: Array<string>;
+};
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
+  data: Board;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get todo() {
+    return this.data.todo;
   }
 
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-  doing = ['test'];
-  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+  get doing() {
+    return this.data.doing;
+  }
+
+  get done() {
+    return this.data.done;
+  }
+  constructor() {
+    this.data = JSON.parse(localStorage.getItem('data') || '');
+  }
+
+  ngOnInit(): void {}
 
   drop(event: CdkDragDrop<string[]>) {
-    console.log('dropped');
-    
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
+
+    localStorage.setItem('data', JSON.stringify(this.data));
   }
 }
-
